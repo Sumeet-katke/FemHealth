745232c74be8fe3655cd07f0896dc1c34e65a409
Sumeetkumar katke <snkatke9874@gmail.com>
2025-06-06 11:45:09 +0530

    Changed URL

--- femhealth/src/Landing/JSX/RegisterModal.jsx
+++ femhealth/src/Landing/JSX/RegisterModal.jsx
@@ -39,7 +39,7 @@ const RegisterModal = ({ onClose }) => {
 
     try {
       const response = await APIFetcher({
-        url: 'http://127.0.0.1:5000/api/register/',
+        url: 'https://femhealth-backend.onrender.com/api/register/',
         method: 'POST',
         body: form,
       });
